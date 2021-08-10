25a59469b69096d5219f65b538e116f9c612ee7b
saifulnizar <saifunizar283@gmail.com>
2021-08-10 20:09:44 +0700

    splashscreen

--- src/pages/SplashScreen/index.js
+++ src/pages/SplashScreen/index.js
@@ -1,12 +1,42 @@
 import React from 'react';
-import { SafeAreaView, View, Text } from 'react-native';
+import { SafeAreaView, View, Text, StyleSheet, Image } from 'react-native';
+
+import COLORS from '../../utils/color';
 
 const SplashScreen = () => {
     return (
-        <SafeAreaView>
-            <Text>This is splashscreen</Text>
-        </SafeAreaView>
+   		<SafeAreaView style={styles.container}>
+   			<View style={styles.block} >
+        		<Image
+              		source={require("../../assets/logo.png")}
+              		style={styles.logo}
+            	/>
+            	<Text style={styles.text} >Simple is better</Text>
+            </View>
+       	</SafeAreaView>
     )
 }
 
-export default SplashScreen
+
+const styles = StyleSheet.create({
+	container : {
+		flex: 1,
+		backgroundColor: COLORS.PRIMARY,
+	},
+	block : {
+		alignItems: 'center' 
+	},
+	logo : {
+		marginTop: 200,
+		width: 200,
+		height: 200,
+	},
+	text :{
+		marginTop: 24,
+		color: COLORS.WHITE,
+		fontSize: 25,
+		lineHeight: 44.8,
+	},
+});
+
+export default SplashScreen;
