6115a2cb1a34e3040e4e87eef2473015d860c39d
saifulnizar <saifulnizar283@gmail.com>
2021-08-15 16:51:29 +0700

    bug:font_ios

--- src/utils/fonts.js
+++ src/utils/fonts.js
@@ -5,8 +5,8 @@ import {Platform} from 'react-native';
 
 export const fonts = {
 
-	NunitoSansReguler  : Platform.OS === 'ios' ? 'NunitoSans Regular' : 'NunitoSans-Regular',
-	NunitoSansSemiBold : Platform.OS === 'ios' ? 'NunitoSans SemiBold' : 'NunitoSans-SemiBold',
-	NunitoSansBold     : Platform.OS === 'ios' ? 'NunitoSans Bold' : 'NunitoSans-Bold',
-	NunitoSansLight    : Platform.OS === 'ios' ? 'NunitoSans Light' : 'NunitoSans-Light'
+	NunitoSansReguler  : Platform.OS === 'ios' ? 'NunitoSans-Regular' : 'NunitoSans-Regular',
+	NunitoSansSemiBold : Platform.OS === 'ios' ? 'NunitoSans-SemiBold' : 'NunitoSans-SemiBold',
+	NunitoSansBold     : Platform.OS === 'ios' ? 'NunitoSans-Bold' : 'NunitoSans-Bold',
+	NunitoSansLight    : Platform.OS === 'ios' ? 'NunitoSans-Light' : 'NunitoSans-Light'
 };
