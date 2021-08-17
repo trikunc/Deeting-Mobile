202d88fe330dfaa7c3e6dc2fefa6ed81fb401e05
trikunc <kurookun@gmail.com>
2021-08-17 21:55:44 +0700

    DEETING-81 Add Tab Navigator

--- src/app.js
+++ src/app.js
@@ -1,26 +1,9 @@
 import React, {Component} from 'react';
 import 'react-native-gesture-handler';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-
-import SplashScreen from './pages/SplashScreen/index.js';
-import Login from './pages/Login/index';
-import Home from './pages/Home/index';
-
-const Stack = createStackNavigator();
+import Router from './route/index';
 
 export default class App extends Component {
   render() {
-    return (
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="SplashScreen">
-          <Stack.Screen
-            name="SplashScreen"
-            component={Login}
-            options={{headerShown: false}}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    );
+    return <Router />;
   }
 }
---
+++ src/route/index.js
@@ -0,0 +1,61 @@
+import React, {useState, useEffect} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator, TransitionPresets} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+// import Login from '../pages/Login/index';
+import TabsScreen from './TabsScreen';
+
+import Landing from '../pages/Login/landing';
+import SignIn from '../pages/Login/signIn';
+import Forgot from '../pages/Login/forgot';
+import ResetPassword from '../pages/Login/forgot/resetPassword';
+import Register from '../pages/Register';
+import SuccesSignUp from '../pages/Register/succesSignUp';
+import ChangePassword from '../pages/Home/ChangePass';
+
+const Stack = createStackNavigator();
+
+function AppNavigation() {
+  const style = {
+    headerShown: false,
+    ...TransitionPresets.SlideFromRightIOS,
+  };
+  const AppStack = () => (
+    <Stack.Navigator initialRouteName="Landing">
+      <Stack.Screen
+        name="TabsScreen"
+        component={TabsScreen}
+        options={{headerShown: false}}
+      />
+
+      <Stack.Screen name="Landing" component={Landing} options={style} />
+      <Stack.Screen name="SignIn" component={SignIn} options={style} />
+      <Stack.Screen name="ForgotPassword" component={Forgot} options={style} />
+      <Stack.Screen
+        name="ResetPassword"
+        component={ResetPassword}
+        options={style}
+      />
+      <Stack.Screen name="Register" component={Register} options={style} />
+      <Stack.Screen
+        name="SuccesSignUp"
+        component={SuccesSignUp}
+        options={style}
+      />
+      <Stack.Screen
+        name="ChangePassword"
+        component={ChangePassword}
+        options={style}
+      />
+    </Stack.Navigator>
+  );
+
+  return (
+    <NavigationContainer>
+      <AppStack />
+    </NavigationContainer>
+  );
+}
+
+export default AppNavigation;
