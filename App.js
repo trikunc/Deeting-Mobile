0f080ef7c5fdb6b9ef0331c099bce976349f3026
saifulnizar <saifulnizar283@gmail.com>
2021-08-31 14:23:19 +0700

    feat:provider_store_auth

--- App.js
+++ App.js
@@ -3,14 +3,14 @@ import 'react-native-gesture-handler';
 import React from 'react';
 
 import { Provider } from 'react-redux';
-import { storeLanguage } from './src/store/index';
+import { store } from './src/store/index';
 
 import RouterApp from './src/app';
 
 const App = ({}) => {
 
   return (
-      <Provider store={storeLanguage}>
+      <Provider store={store}>
         <RouterApp />
       </Provider>
   );
